a7c6b412bb63e3168d8afc865789700117bff7cc
lobrien <lobrien@knowing.net>
2018-09-18 13:12:57 -1000

    Beginning to create Report (model)

---
+++ MarineDebrisReporting/Model.fs
@@ -0,0 +1,45 @@
+﻿namespace MarineDebrisReporting
+
+open System
+open Xamarin.Essentials
+
+module Model =
+
+    type FileSystemUrl = string
+
+    type DebrisWeightT = 
+        | Light
+        | Totable
+        | Liftable
+        | Heavy
+        | MachineOnly
+
+    type DebrisSizeT = 
+        | TrashElement
+        | TrashPile 
+        | DebrisElement
+        | DebrisPile 
+        | Line
+        | Other 
+
+    type MaterialT =
+        | Plastic
+        | Wood
+        | Metal
+        | Fiberglas 
+        | FishingGear 
+        | Amalgam
+        | Other
+
+    type Report =
+        {
+            Timestamp : DateTime
+            Location : Option<Location>
+            Size : Option<DebrisSizeT>
+            Material : Option<MaterialT>
+            Weight : Option<DebrisWeightT>
+            Notes : Option<String> 
+            Picture : Option<unit -> FileSystemUrl>
+        }
+        
+
